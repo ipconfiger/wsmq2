e0cbe45a33f34f2c74106d8b0066b5af6aaf4853
ipconfiger <superpowerlee@gmail.com>
2023-04-21 09:49:46 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,131 @@
+use std::env;
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web_actors::ws;
+use serde::Serialize;
+mod websocks;
+
+#[macro_use] 
+extern crate lazy_static;
+
+lazy_static! {
+    static ref ID_GENERATOR:websocks::IdGenerator = websocks::IdGenerator::new(0);
+}
+struct AppState {
+    db: sled::Db,
+    range_idx: sled::Tree,
+    day_idx: sled::Tree,
+    main_idx: sled::Tree,
+    nonce_idx: sled::Tree
+}
+
+async fn index(req: HttpRequest, stream: web::Payload, data: web::Data<AppState>) -> Result<HttpResponse, Error> {
+    let client_id: &str = req.match_info().get("cid").unwrap();
+    let db = data.db.clone();
+    let resp = ws::start(websocks::WsSession {
+        topics:Some(vec![]),
+        db,
+        client_id: client_id.to_string(),
+        offset: 0,
+        range_idx: data.range_idx.clone(),
+        day_idx: data.day_idx.clone(),
+        main_idx: data.main_idx.clone(),
+        nonce_idx: data.nonce_idx.clone(),
+        id_generator: ID_GENERATOR.clone()
+    }, &req, stream);
+    println!("{:?}", resp);
+    resp
+}
+
+#[derive(Serialize)] 
+struct Status {
+    retain_messages: usize,
+    disk_size: u64,
+    last_nonce: u64
+}
+
+async fn status_handler(_req: HttpRequest, data: web::Data<AppState>) -> impl Responder{
+    let disk_size = match data.db.size_on_disk(){
+        Ok(sz)=>sz,
+        Err(_)=>0
+    };
+    
+    let retain_messages = data.range_idx.len();
+
+    let last_nonce = match data.range_idx.last() {
+        Ok(Some((k, _v)))=>u64::from_be_bytes(k.to_vec().try_into().unwrap()),
+        Err(_e)=>0,
+        Ok(None)=>0
+    };
+    
+    let status = Status{
+        retain_messages,
+        disk_size,
+        last_nonce
+    };
+    
+    let json = serde_json::to_string(&status).unwrap();
+    HttpResponse::Ok().body(json)
+}
+
+async fn trim_handler(req: HttpRequest, data: web::Data<AppState>) -> impl Responder{
+    let offset: &str = req.match_info().get("offset").unwrap();
+    let off_i32: i32 = offset.parse::<i32>().unwrap();
+    let target_timestamp = websocks::today_ts() - (3600 * 24 * (off_i32 + 1)) as i64;
+    println!("tar ts: {target_timestamp:?}");
+    if let Ok(Some(v)) = data.day_idx.get(websocks::i64to_vec(target_timestamp)){
+        println!("Got nonce:{v:?}");
+        for item in data.range_idx.range(..v){
+            if let Ok((rkey, data_key)) = item {
+                let data_key_for_split = data_key.clone();
+                println!("Matched key:「{data_key:?}」");
+                if let Ok(_) = data.db.remove(data_key){
+                    println!("removed from storage");
+                }
+                let str_key = String::from_utf8(data_key_for_split.to_vec().try_into().unwrap()).unwrap();
+                let topic = str_key.split('-').next().unwrap();
+                let nonce = websocks::vectu64(rkey.to_vec());
+                let main_key = websocks::make_key(topic, nonce);
+                if let Ok(_) = data.main_idx.remove(main_key){
+                    println!("removed from index");
+                }
+                if let Ok(_) = data.range_idx.remove(rkey){
+                    println!("removed from index");
+                }
+            }
+        }
+    }
+    let resp: websocks::ErrResp = websocks::ErrResp{rs:true, detail:"".to_string()};
+    let json = serde_json::to_string(&resp).unwrap();
+    HttpResponse::Ok().body(json)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    println!("args:{args:?}");
+    let port = if args.len() > 1usize {
+        args[1].parse::<u16>().unwrap()
+    }else{
+        8080
+    };
+    let db = sled::open("db.sled").unwrap();
+    let r_idx = db.open_tree("range").unwrap();
+    let d_idx = db.open_tree("d_idx").unwrap();
+    let m_idx = db.open_tree("main_idx").unwrap();
+    let nonce_idx = db.open_tree("uid_to_nonce_idx").unwrap();
+
+    if let Ok(Some((k, _v))) = r_idx.last(){
+        let last_id = u64::from_be_bytes(k.to_vec().try_into().unwrap());
+        println!("last id:{}", last_id);
+        ID_GENERATOR.init_with(last_id);
+    }
+    let app_state = web::Data::new(AppState { db, range_idx: r_idx, day_idx: d_idx, main_idx: m_idx, nonce_idx});
+    HttpServer::new(move || App::new()
+                            .route("/ws/{cid}", web::get().to(index))
+                            .route("/api/status", web::get().to(status_handler))
+                            .route("/api/trim/{offset}/days", web::get().to(trim_handler))
+                            .app_data(app_state.clone()))
+        .bind(("0.0.0.0", port))?
+        .run()
+        .await
+}
---
+++ src/websocks.rs
@@ -0,0 +1,270 @@
+use actix::{Actor, StreamHandler};
+use actix::prelude::*;
+use actix_web_actors::ws;
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+use chrono::NaiveDate;
+use chrono::prelude::*;
+use sled::IVec;
+
+#[derive(Debug, PartialEq)]
+struct TransactionError;
+
+pub fn today_ts() -> i64{
+    let today = NaiveDate::from_ymd_opt(
+        chrono::Local::now().year(),
+        chrono::Local::now().month(),
+        chrono::Local::now().day()
+    ).unwrap();
+    let dt = chrono::NaiveDateTime::new(today, chrono::NaiveTime::from_hms_opt(0, 0, 0).unwrap());
+    let timestamp = dt.timestamp();
+    timestamp
+}
+
+pub fn i64to_vec(n: i64) -> Vec<u8> {
+    Vec::from(n.to_be_bytes())
+}
+
+pub fn vectu64(vc: Vec<u8>) -> u64 {
+    u64::from_be_bytes(vc.try_into().unwrap())
+}
+
+pub fn make_key(topic: &str, nonce: u64) -> IVec {
+    let mut nonce_vec = Vec::from(nonce.to_be_bytes());
+    let mut topic_vec = Vec::from(topic.as_bytes());
+    topic_vec.append(&mut nonce_vec);
+    IVec::from(topic_vec)
+}
+
+#[derive(Debug, Clone)]
+pub struct IdGenerator {
+    max_id: Arc<Mutex<u64>>
+}
+
+impl IdGenerator {
+    pub fn new(init_id: u64) -> IdGenerator {
+        IdGenerator { 
+            max_id: Arc::new(Mutex::new(init_id))
+        }
+    }
+    
+    pub fn init_with(&self, init_id: u64) {
+        let mut max_id = self.max_id.lock().unwrap();
+        *max_id = init_id;
+    }
+
+    pub fn gen_id(&self) -> u64 {
+        let mut max_id = self.max_id.lock().unwrap();
+        *max_id += 1;
+        let new_id = *max_id;
+        new_id
+    }
+}
+
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Message {
+    uid: String,
+    topic: Option<String>,
+    payload: Option<String>,
+    key: Option<String>,
+    cmd: Option<String>,
+    params: Option<Vec<String>>,
+    offset: Option<u64>,
+    nonce: Option<u64>
+}
+
+impl Message {
+    fn got_topic(&mut self) -> Option<String>{
+        if let Some(topic) = &self.topic{
+            Some(topic.clone())
+        }else{
+            None
+        }
+    }
+    fn got_cmd(&mut self) -> Option<String>{
+        if let Some(cmd) = &self.cmd{
+            Some(cmd.clone())
+        }else{
+            None
+        }
+    }
+    fn got_offset(&mut self) -> Option<u64>{
+        if let Some(offset) = &self.offset{
+            Some(*offset)
+        }else{
+            None
+        }
+    }
+    fn got_params(&mut self) -> Option<Vec<String>>{
+        if let Some(params) = &self.params{
+            Some(params.clone())
+        }else{
+            None
+        }
+    }
+}
+
+
+#[derive(Serialize, Deserialize)]
+pub struct Command {
+    cmd: String,
+    params: Vec<String>,
+}
+
+
+#[derive(Serialize, Deserialize)]
+pub struct ErrResp {
+    pub rs: bool,
+    pub detail: String
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+struct InnerMessage(String);
+
+pub struct WsSession{
+    pub client_id: String,
+    pub topics: Option<Vec<String>>,
+    pub db: sled::Db,
+    pub offset: u64,
+    pub range_idx: sled::Tree,
+    pub day_idx: sled::Tree,
+    pub main_idx: sled::Tree,
+    pub nonce_idx: sled::Tree,
+    pub id_generator: IdGenerator
+}
+
+impl Actor for WsSession {
+    type Context = ws::WebsocketContext<Self>;
+    fn started(&mut self, ctx: &mut Self::Context) {
+        ctx.text("{\"rs\":true,\"detail\":\"connected\"}");
+        ctx.run_interval(Duration::from_millis(200), |act, ctx|{
+            act.every_200_ms(ctx);
+        });
+       
+    }
+
+    fn stopped(&mut self, _ctx: &mut Self::Context) {
+        println!("client:{} disconnected", self.client_id);
+    }
+
+}
+
+
+/// Handler for ws::Message message
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WsSession {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => {
+                match serde_json::from_str::<Message>(&text){
+                    Ok(msg)=>{
+                        println!("Got Event to:{:?}", msg);
+                        let mut msg = msg;
+                        self.process_message(&mut msg);
+                    }
+                    Err(err)=>{
+                        println!("Invalid Message:{} error:{}", text, err);
+                        ctx.text(serde_json::to_string(&ErrResp{rs: false, detail: format!("Invalid json:{err}")}).unwrap())
+                    }
+                };
+            },
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            Ok(ws::Message::Close(_))=>{ctx.close(Some(ws::CloseReason{code:ws::CloseCode::Normal, description: Some("reset by peer".to_string())}));}
+            _ => (),
+        }
+    }
+}
+
+impl WsSession {
+    fn every_200_ms(&mut self, ctx: &mut <WsSession as Actor>::Context){
+        // execute every 200ms
+        if let Some(topics) = &self.topics{
+            // if subscribe some topics
+            for topic in topics{
+                let fetch_flag_min = make_key(topic.as_str(), self.offset);
+                let fetch_flag_max = make_key(topic.as_str(), u64::MAX);
+                for item in self.main_idx.range(fetch_flag_min..fetch_flag_max) {
+                    if let Ok((_k, data_key)) = item{
+                        let data_key2 = data_key.clone();
+                        if let Ok(Some(data)) = self.db.get(data_key){
+                            if let Ok(json_text) = String::from_utf8(data.to_vec()){
+                                if let Ok(Some(nonce_ivec)) = self.nonce_idx.get(data_key2){
+                                    let offset = vectu64(nonce_ivec.to_vec());
+                                    self.offset = offset + 1;
+                                }
+                                let debug_msg = json_text.clone();
+                                ctx.text(json_text);
+                                println!("Message {} dispached to {}", debug_msg, topic)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    fn process_message(&mut self, message: &mut Message) {
+        if let Some(cmd) = message.got_cmd() {
+            // this is a command
+            let command = cmd.clone();
+            let command_str = command.as_str();
+            let params = if let Some(p) = message.got_params() { p } else { vec![] };
+            let offset = if let Some(off) = message.got_offset() { off } else { 0 as u64};
+            //self.process_command(command_str, params, offset);
+            if command_str == "subscribe" {
+                self.topics = Some(params);
+                if offset > 0{
+                    self.offset = offset;
+                }
+            }
+        }
+        if let Some(_) = message.got_topic() {
+            // if got topic, it's a message, run dispatch!
+            self.dispatch_message(message);
+        }
+    }
+
+    fn dispatch_message(&mut self, message: &mut Message) {
+        let nonce = self.id_generator.gen_id();
+        message.nonce = Some(nonce);
+        let message_topic = message.got_topic().unwrap();
+        let key = format!("{}-{}", message_topic, message.uid);
+        let val = serde_json::to_string(message).unwrap();
+        let idx_key = Vec::from(nonce.to_be_bytes());
+        let today_timestamp_vec = i64to_vec(today_ts());
+        // update today's last nonce index
+        let new_idx_key = idx_key.clone();
+        if let Ok(_k) = self.day_idx.insert(today_timestamp_vec, idx_key){
+            println!("update today's last nonce success!");
+            if let Ok(_) = self.range_idx.insert(new_idx_key, key.as_bytes()){
+                println!("update range index success!");
+                let main_key = make_key(message_topic.as_str(), nonce);
+                if let Ok(_) = self.main_idx.insert(main_key, key.as_bytes()) {
+                    println!("update main index success!");
+                    let key2 = key.clone();
+                    if let Ok(_) = self.db.insert(key, val.into_bytes()){
+                        println!("insert data success!");
+                        if let Ok(_) = self.nonce_idx.insert(key2, IVec::from(nonce.to_be_bytes().to_vec())){
+                            println!("insert nonce idx success!");
+                        }else{
+                            println!("insert nonce idx faild!");
+                        }
+                        
+                    }else{
+                        eprintln!("insert data faild!");
+                    }
+                }else{
+                    eprintln!("update main index faild!");
+                }
+            }else{
+                eprintln!("update range index faild!");
+            }
+        }else{
+            eprintln!("update today's last nonce faild!");
+        };
+    }
+    
+}
